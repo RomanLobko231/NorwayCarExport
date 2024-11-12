56015b58c3de920bfc1ac4f1b9c2d58d378ee91b
Roman <roomen817@gmail.com>
2024-11-12 14:14:56 +0100

    + top snapping

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./ui/Navbar";
 import Home from "./pages/Home";
 import Footer from "./ui/Footer";
 import RegisterSeller from "./pages/RegisterSeller";
+import ScrollToTop from "./tools/ScrollToTop";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="absolute inset-0 bg-lighthouse opacity-90"></div>
       <div className="relative z-10 flex min-h-screen flex-col">
         <Navbar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} exact={true} />
           <Route path="*" element={<Home />} exact={true} />
